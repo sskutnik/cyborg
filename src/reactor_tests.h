1bc0d95be7a86040a22c46af255d9b3347821109
Jenny <jlittel1@vols.utk.edu>
2016-03-15 09:02:46 -0500

    add library path state variable, PyNE nuclide id tranformations

---
+++ src/reactor_tests.h
@@ -0,0 +1,40 @@
+#ifndef REACTOR_TESTS_H_
+#define REACTOR_TESTS_H_
+
+#include <gtest/gtest.h>
+
+#include "reactor.h"
+
+#include "context.h"
+#include "facility_tests.h"
+#include "agent_tests.h"
+
+namespace reactor {
+// - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+class reactorTest : public ::testing::Test {
+ protected:
+  cyclus::TestContext tc_;
+  cyborg::reactor* src_facility_;
+
+  virtual void SetUp();
+  virtual void TearDown();
+  // void InitParameters();
+  // void SetUpStorage();
+  // void TestInitState(storage::Storage* fac);
+  // void TestAddMat(storage::Storage* fac, 
+  //     cyclus::Material::Ptr mat);
+  // void TestBuffers(storage::Storage* fac, double inv, double 
+  //     proc, double ready, double stocks);
+  // void TestStocks(storage::Storage* fac, cyclus::CompMap v);
+  // void TestReadyTime(storage::Storage* fac, int t);
+  // void TestCurrentCap(storage::Storage* fac, double inv);
+
+  // std::vector<std::string> in_c1, out_c1;
+  // std::string in_r1;
+
+  // int residence_time;
+  // double throughput, max_inv_size;
+  // bool discrete_handling;
+};
+} // namespace reactor
+#endif // REACTOR_TESTS_H_
